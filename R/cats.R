f84d133ae3c46e68034dd8f0cb70d3cb408cf4d0
Celeste Valdivia <cvaldi@uw.edu>
2023-02-22 12:02:27 -0800

    initial commit

---
+++ R/cats.R
@@ -0,0 +1,14 @@
+cats <- function(love = TRUE) {
+  if(love == TRUE) {
+    msg <- "I love cats!"
+  }
+  else {
+    msg <- "I am not a cat person."
+  }
+  return(print(msg))
+}
+
+cats(TRUE)
+cats(1)
+cats(0)
+cats
